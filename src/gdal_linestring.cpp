naturalatlas/node-gdal

commit 0315066618ac38677904672507ccd556075bc123

Updated documentation.

src/gdal_linestring.cpp
@@ -156,37 +156,6 @@ NAN_METHOD(LineString::toString)
 NODE_WRAPPED_METHOD_WITH_RESULT(LineString, getLength, Number, get_Length);
 
 /**
- * Returns the number of the points that make up the line string.
- *
- * @method getNumPoints
- * @return Number
- */
-NODE_WRAPPED_METHOD_WITH_RESULT(LineString, getNumPoints, Integer, getNumPoints);
-
-/**
- * Returns the point at an index.
- *
- * @method getPoint
- * @param {integer} index 0-based index
- * @return {gdal.Point}
- */
-NAN_METHOD(LineString::getPoint)
-{
-	NanScope();
-
-	LineString *geom = ObjectWrap::Unwrap<LineString>(args.This());
-
-	OGRPoint *pt = new OGRPoint();
-	int i;
-
-	NODE_ARG_INT(0, "i", i);
-
-	geom->this_->getPoint(i, pt);
-
-	NanReturnValue(Point::New(pt));
-}
-
-/**
  * Returns the point at the specified distance along the line string.
  *
  * @method value
@@ -207,35 +176,6 @@ NAN_METHOD(LineString::value)
 	geom->this_->Value(dist, pt);
 
 	NanReturnValue(Point::New(pt));
-}
-
-NAN_METHOD(LineString::addPoint)
-{
-	NanScope();
-
-	LineString *geom = ObjectWrap::Unwrap<LineString>(args.This());
-
-	if (args[0]->IsNumber()) {
-
-		double x = 0, y = 0, z = 0;
-		NODE_ARG_DOUBLE(0, "x", x);
-		NODE_ARG_DOUBLE(1, "y", y);
-		NODE_ARG_DOUBLE_OPT(2, "z", z);
-		if (args.Length() < 3) {
-			geom->this_->addPoint(x, y);
-		} else {
-			geom->this_->addPoint(x, y, z);
-		}
-
-	} else {
-
-		Point* pt;
-		NODE_ARG_WRAPPED(0, "point", Point, pt);
-		geom->this_->addPoint(pt->get());
-
-	}
-
-	NanReturnUndefined();
 }
 
 /**

src/collections/gdal_drivers.cpp
@@ -36,6 +36,11 @@ GDALDrivers::GDALDrivers()
 GDALDrivers::~GDALDrivers()
 {}
 
+/**
+ * An collection of all {{#crossLink "gdal.Driver"}}drivers{{/crossLink}} registered with GDAL.
+ *
+ * @class gdal.GDALDrivers
+ */
 NAN_METHOD(GDALDrivers::New)
 {
 	NanScope();
@@ -74,6 +79,16 @@ NAN_METHOD(GDALDrivers::toString)
 	NanReturnValue(NanNew("GDALDrivers"));
 }
 
+/**
+ * Returns a driver with the specified name.
+ *
+ * Note: Prior to GDAL2.x there is a separate driver for vector VRTs and raster VRTs.
+ *       Use `"VRT:vector"` to fetch the vector VRT driver and `"VRT:raster"` to fetch the raster VRT driver.
+ * 
+ * @method get
+ * @param {Integer|string} index 0-based index or driver name
+ * @return {gdal.Driver}
+ */
 NAN_METHOD(GDALDrivers::get)
 {
 	NanScope();
@@ -148,6 +163,12 @@ NAN_METHOD(GDALDrivers::get)
 	NanReturnValue(NanNull());
 }
 
+/**
+ * Returns an array with the names of all the drivers registered with GDAL.
+ *
+ * @method getNames
+ * @return {string[]}
+ */
 NAN_METHOD(GDALDrivers::getNames)
 {
 	NanScope();
@@ -185,6 +206,12 @@ NAN_METHOD(GDALDrivers::getNames)
 	NanReturnValue(driver_names);
 }
 
+/**
+ * Returns the number of drivers registered with GDAL.
+ *
+ * @method count
+ * @return {Integer}
+ */
 NAN_METHOD(GDALDrivers::count)
 {
 	NanScope();

src/collections/layer_features.cpp
@@ -135,13 +135,13 @@ NAN_METHOD(LayerFeatures::first)
 }
 
 /**
- * Returns the next feature in the layer.
+ * Returns the next feature in the layer. Returns null if no more features.
  *
  * @example
  * ```
  * while (feature = layer.features.next()) { ... }```
  *
- * @method first
+ * @method next
  * @return {gdal.Feature}
  */
 NAN_METHOD(LayerFeatures::next)
@@ -161,7 +161,14 @@ NAN_METHOD(LayerFeatures::next)
 }
 
 /**
- * Adds a feature to the layer.
+ * Adds a feature to the layer. The feature should be created using the current layer as the definition.
+ *
+ * @example
+ * ```
+ * var feature = new gdal.Feature(layer);
+ * feature.setGeometry(new gdal.Point(0, 1));
+ * feature.fields.set('name', 'somestring');
+ * layer.features.add(feature);```
  *
  * @method add
  * @throws Error
@@ -192,8 +199,7 @@ NAN_METHOD(LayerFeatures::add)
 /**
  * Returns the number of features in the layer.
  *
- * @method add
- * @throws Error
+ * @method count
  * @param {Boolean} [force=true]
  * @return {Integer} Number of features in the layer.
  */

src/collections/layer_fields.cpp
@@ -84,7 +84,6 @@ NAN_METHOD(LayerFields::toString)
 /**
  * Returns the number of fields.
  *
- * @throws Error
  * @method count
  * @return {Integer}
  */
@@ -111,7 +110,6 @@ NAN_METHOD(LayerFields::count)
 /**
  * Find the index of field in the layer.
  *
- * @throws Error
  * @method indexOf
  * @param {String} field
  * @return {Integer} Field index, or -1 if the field doesn't exist

src/collections/linestring_points.cpp
@@ -245,7 +245,7 @@ NAN_METHOD(LineStringPoints::set)
 }
 
 /**
- * Adds point(s) to the line string.
+ * Adds point(s) to the line string. Also accepts any object with an x and y property.
  *
  * @example
  * ```
@@ -257,7 +257,7 @@ NAN_METHOD(LineStringPoints::set)
  *
  * @method add
  * @throws Error
- * @param {gdal.Point|Array} point(s)
+ * @param {gdal.Point|object|Array} point(s)
  */
 NAN_METHOD(LineStringPoints::add)
 {

src/collections/rasterband_pixels.cpp
@@ -40,6 +40,14 @@ RasterBandPixels::~RasterBandPixels()
 /**
  * A representation of a {{#crossLink "gdal.RasterBand"}}RasterBand{{/crossLink}}'s pixels.
  *
+ * 
+ * Note: Typed arrays should be created with an external ArrayBuffer for versions of node >= 0.11
+ * ```
+ * var n = 16*16;
+ * var data = new Float32Array(new ArrayBuffer(n*4));
+ * //read data into the existing array
+ * band.pixels.read(0,0,16,16,data);```
+ *
  * @class gdal.RasterBandPixels
  */
 NAN_METHOD(RasterBandPixels::New)

src/gdal_algorithms.cpp
@@ -24,10 +24,10 @@ void Algorithms::Initialize(Handle<Object> target)
  * @static
  * @for gdal
  * @param {Object} options
- * @param {gdal.RasterBand} options.src If provided, this band will be updated in-place.
- * @param {gdal.RasterBand} options.mask Mask band
- * @param {Number} options.searchDist
- * @param {integer} options.smoothingIterations
+ * @param {gdal.RasterBand} options.src This band to be updated in-place.
+ * @param {gdal.RasterBand} [options.mask] Mask band
+ * @param {Number} options.searchDist The maximum distance (in pixels) that the algorithm will search out for values to interpolate.
+ * @param {integer} [options.smoothingIterations=0] The number of 3x3 average filter smoothing iterations to run after the interpolation to dampen artifacts.
  */
 NAN_METHOD(Algorithms::fillNodata)
 {
@@ -71,12 +71,12 @@ NAN_METHOD(Algorithms::fillNodata)
  * @param {Object} options
  * @param {gdal.RasterBand} options.src
  * @param {gdal.Layer} options.dst
- * @param {Number} options.base
- * @param {Number} options.interval
- * @param {Number[]} options.fixedLevels
- * @param {Number} options.noData
- * @param {integer} options.idField
- * @param {integer} options.elevField
+ * @param {Number} [options.offset=0] The "offset" relative to which contour intervals are applied. This is normally zero, but could be different. To generate 10m contours at 5, 15, 25, ... the offset would be 5.
+ * @param {Number} [options.interval=100] The elevation interval between contours generated.
+ * @param {Number[]} [options.fixedLevels] A list of fixed contour levels at which contours should be generated. Overrides interval/base options if set.
+ * @param {Number} [options.nodata] The value to use as a "nodata" value. That is, a pixel value which should be ignored in generating contours as if the value of the pixel were not known.
+ * @param {integer} [options.idField] A field index to indicate where a unique id should be written for each feature (contour) written.
+ * @param {integer} [options.elevField] A field index to indicate where the elevation value of the contour should be written.
  */
 NAN_METHOD(Algorithms::contourGenerate)
 {
@@ -92,14 +92,14 @@ NAN_METHOD(Algorithms::contourGenerate)
 	int n_fixed_levels = 0;
 	int use_nodata = 0;
 	double nodata = 0;
-	int id_field, elev_field;
+	int id_field = -1, elev_field = -1;
 
 	NODE_ARG_OBJECT(0, "options", obj);
 
 	NODE_WRAPPED_FROM_OBJ(obj, "src", RasterBand, src);
 	NODE_WRAPPED_FROM_OBJ(obj, "dst", Layer, dst);
-	NODE_INT_FROM_OBJ(obj, "idField", id_field);
-	NODE_INT_FROM_OBJ(obj, "elevField", elev_field);
+	NODE_INT_FROM_OBJ_OPT(obj, "idField", id_field);
+	NODE_INT_FROM_OBJ_OPT(obj, "elevField", elev_field);
 	NODE_DOUBLE_FROM_OBJ_OPT(obj, "interval", interval);
 	NODE_DOUBLE_FROM_OBJ_OPT(obj, "offset", base);
 	if(obj->HasOwnProperty(NanNew("fixedLevels"))){
@@ -142,7 +142,7 @@ NAN_METHOD(Algorithms::contourGenerate)
  * @param {gdal.RasterBand} options.dst Output raster band. It may be the same as src band to update the source in place.
  * @param {gdal.RasterBand} [options.mask] All pixels in the mask band with a value other than zero will be considered suitable for inclusion in polygons.
  * @param {Number} options.threshold Raster polygons with sizes smaller than this will be merged into their largest neighbour.
- * @param {integer} options.connectedness Either 4 indicating that diagonal pixels are not considered directly adjacent for polygon membership purposes or 8 indicating they are.
+ * @param {integer} [options.connectedness=4] Either 4 indicating that diagonal pixels are not considered directly adjacent for polygon membership purposes or 8 indicating they are.
  */
 NAN_METHOD(Algorithms::sieveFilter)
 {
@@ -242,8 +242,8 @@ NAN_METHOD(Algorithms::checksumImage)
  * @param {gdal.Layer} options.dst
  * @param {gdal.RasterBand} [options.mask]
  * @param {integer} options.pixValField The attribute field index indicating the feature attribute into which the pixel value of the polygon should be written.
- * @param {integer} options.connectedness Either 4 indicating that diagonal pixels are not considered directly adjacent for polygon membership purposes or 8 indicating they are.
- * @param {Boolean} options.useFloats Use floating point buffers instead of int buffers.
+ * @param {integer} [options.connectedness=4] Either 4 indicating that diagonal pixels are not considered directly adjacent for polygon membership purposes or 8 indicating they are.
+ * @param {Boolean} [options.useFloats=false] Use floating point buffers instead of int buffers.
  */
 NAN_METHOD(Algorithms::polygonize)
 {

src/gdal_coordinate_transformation.cpp
@@ -50,10 +50,11 @@ CoordinateTransformation::~CoordinateTransformation()
 /**
  * Object for transforming between coordinate systems.
  *
+ * @throws Error
  * @constructor
  * @class gdal.CoordinateTransformation
  * @param {gdal.SpatialReference} source
- * @param {gdal.SpatialReference|gdal.Dataset} target If a Dataset, the conversion will represent a conversion to pixel coordinates.
+ * @param {gdal.SpatialReference|gdal.Dataset} target If a raster Dataset, the conversion will represent a conversion to pixel coordinates.
  */
 NAN_METHOD(CoordinateTransformation::New)
 {

liubomyr191/gdal-with-node

commit 3ab03ce70a7c0a5cdedaf6c50ebf1bed7e1bb2c0

Fixed regression: removed references to addPoint, getPoint, getNumPoints. Broke in https://github.com/naturalatlas/node-gdal/commit/0315066618ac38677904672507ccd556075bc123

src/gdal_linestring.cpp
@@ -21,9 +21,6 @@ void LineString::Initialize(Handle<Object> target)
 	lcons->SetClassName(NanNew("LineString"));
 
 	NODE_SET_PROTOTYPE_METHOD(lcons, "toString", toString);
-	NODE_SET_PROTOTYPE_METHOD(lcons, "addPoint", addPoint);
-	NODE_SET_PROTOTYPE_METHOD(lcons, "getPoint", getPoint);
-	NODE_SET_PROTOTYPE_METHOD(lcons, "getNumPoints", getNumPoints);
 	NODE_SET_PROTOTYPE_METHOD(lcons, "getLength", getLength);
 	NODE_SET_PROTOTYPE_METHOD(lcons, "value", value);
 

src/gdal_linestring.hpp
@@ -29,10 +29,7 @@ public:
 	static Handle<Value> New(OGRLineString *geom);
 	static Handle<Value> New(OGRLineString *geom, bool owned);
 	static NAN_METHOD(toString);
-	static NAN_METHOD(addPoint);
-	static NAN_METHOD(getPoint);
 	static NAN_METHOD(getLength);
-	static NAN_METHOD(getNumPoints);
 	static NAN_METHOD(value);
 
 	static NAN_GETTER(pointsGetter);